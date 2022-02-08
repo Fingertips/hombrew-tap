9e199d2a2dccdc35384747807394ece6652ff533
Manfred Stienstra <manfred@fngtps.com>
2022-02-08 14:27:07 +0100

    Bump Teletube to version 0.0.8.

--- Formula/teletube.rb
+++ Formula/teletube.rb
@@ -1,9 +1,9 @@
 class Teletube < Formula
   desc 'Command line interface for interacting with the SWITCHtube web service'
   homepage 'https://github.com/Fingertips/teletube'
-  url 'https://github.com/Fingertips/teletube/archive/refs/tags/v0.0.7.tar.gz'
-  version '0.0.7'
-  sha256 'cab9323e70da980efb9afb33edf216a68a8a167e4d4686c6252a27f43445741b'
+  url 'https://github.com/Fingertips/teletube/archive/refs/tags/v0.0.8.tar.gz'
+  version '0.0.8'
+  sha256 'f7e008ba75f095a607390fd0714fb04baa618e7c407ee6401d1db867e14dd0d8'
   head 'https://github.com/Fingertips/teletube.git'
 
   depends_on 'crystal-lang' => :build
