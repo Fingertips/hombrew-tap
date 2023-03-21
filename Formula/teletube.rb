80a592b9f164a8c131b9393733aa9ce1401a6113
Manfred Stienstra <manfred@fngtps.com>
2023-03-21 13:54:29 +0100

    Update Teletube to v0.1.0.

--- Formula/teletube.rb
+++ Formula/teletube.rb
@@ -1,9 +1,9 @@
 class Teletube < Formula
   desc 'Command line interface for interacting with the SWITCHtube web service'
   homepage 'https://github.com/Fingertips/teletube'
-  url 'https://github.com/Fingertips/teletube/archive/refs/tags/v0.0.8.tar.gz'
-  version '0.0.8'
-  sha256 '0efcf4474b7d36eb99e7e6fbf687c6d2ceab8f5e55d7bb2e6de93c779465df0f'
+  url 'https://github.com/Fingertips/teletube/archive/refs/tags/v0.1.0.tar.gz'
+  version '0.1.0'
+  sha256 '7cfe1070f6e227c924aa01507b55cf29aa3790dd538324f71b6f6fd672052e49'
   head 'https://github.com/Fingertips/teletube.git'
 
   depends_on 'crystal-lang' => :build
